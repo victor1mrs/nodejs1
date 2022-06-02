cdb4c7e5a7278fa3fbcfcbe06737a56b81f60cf2
Victor Regueira <victor1mrs@hotmail.com>
2022-06-02 09:58:30 -0300

    formulario de registro completo

--- app.js
+++ app.js
@@ -5,11 +5,78 @@ const prisma = new PrismaClient()
 const app = express();
 
 const port = 3001;
+
+//seteo urlenconde para capturar los datos del formulario
+app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
+//invoco a dotenv
+const dotenv = require('dotenv');
+const bcryptjs = require('bcryptjs');
+dotenv.config({path:'./.env'});
 
+//el directorio public
+app.use('/resources', express.static('public'));
+app.use('/resources', express.static(__dirname + '/public'));
+
+//establezco el motor de plantillas
+app.set('view engine', 'ejs');
+
+//invoco a bcryptjs
+const bcriptjs = require('bcryptjs');
+
+//variables de session
+const session = require('express-session')
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+
+//establezco las rutas
 app.get('/', (req, res)=>{
-  res.send('HOLA MUNDO')
+  res.render('index', {msg:'Esto es un mensaje de NODE'})
+})
+app.get('/login', (req, res)=>{
+  res.render('login')
+})
+app.get('/register', (req, res)=>{
+  res.render('register')
+})
+
+//registro de usuarios
+app.post('/register', async(req, res)=>{
+  const user = req.body.user;
+  const name = req.body.name;
+  const roll = req.body.rol;
+  const pass = req.body.pass;
+  let passwordHash = await bcriptjs.hash(pass, 8);
+  try{
+    const result = await prisma.users.create({
+      data: {
+        user, name, roll, pass: passwordHash
+      }
+    })
+    res.render('register', {
+      alert: true,
+      alertTitle: 'Registration',
+      alertMessage: 'Successful Registration!',
+      alertIcon: 'success',
+      showConfirmButton: false,
+      timer: 1500,
+      ruta: ''
+    })
+  } catch(e){
+    if (e instanceof prisma.PrismaClientKnownRequestError) {
+      // The .code property can be accessed in a type-safe manner
+      if (e.code === 'P2002') {
+        console.log(
+          'There is a unique constraint violation, a new user cannot be created with this email'
+        )
+      }
+    }
+    throw e
+  }
 })
 
 //Mostrar todos los registros
@@ -32,6 +99,15 @@ app.put('/post/:id', async(req, res)=>{
 app.delete('/post/:id', async(req, res) => {
   const {id} = req.params
   const post = await prisma.post.delete({
+    where: {id: Number(id)}
+  })
+  res.json('Eliminado')
+})
+
+//prototipo borrar usuario
+app.delete('/delete/:id', async(req, res) => {
+  const {id} = req.params
+  const user = await prisma.users.delete({
     where: {id: Number(id)}
   })
   res.json('Eliminado')
--- prisma/migrations/20220601133146_init/migration.sql
+++ prisma/migrations/20220601133146_init/migration.sql
@@ -6,3 +6,14 @@ CREATE TABLE `Post` (
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `user` VARCHAR(255) NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `roll` VARCHAR(100) NOT NULL,
+    `pass` VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
